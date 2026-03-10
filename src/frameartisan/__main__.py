0f35e01fea40234ed3a170a821edb70b07c7f3a9
asomoza <somoza.alvaro@gmail.com>
2026-03-10 14:35:14 -0300

    fix dll order for windows

--- src/frameartisan/__main__.py
+++ src/frameartisan/__main__.py
@@ -1,7 +1,21 @@
+import ctypes
+import glob
+import importlib.util
 import logging
 import logging.config
 import os
 import sys
+
+
+# Preload torch DLLs before PyQt6 to avoid DLL search order conflicts on Windows
+_torch_spec = importlib.util.find_spec("torch")
+if _torch_spec and _torch_spec.origin:
+    _torch_lib = os.path.join(os.path.dirname(_torch_spec.origin), "lib")
+    os.add_dll_directory(_torch_lib)
+    _kernel32 = ctypes.WinDLL("kernel32.dll", use_last_error=True)
+    _kernel32.LoadLibraryW.restype = ctypes.c_void_p
+    for _dll in sorted(glob.glob(os.path.join(_torch_lib, "*.dll"))):
+        _kernel32.LoadLibraryW(_dll)
 
 from frameartisan.app.frameartisan_application import FrameArtisanApplication
 from frameartisan.app.logging_conf import logging_config
